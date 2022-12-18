db8ea1b91ebb864c7aa598ac326fa326ab095a9c
Saiful Islam <codersaiful@gmail.com>
2022-12-18 12:11:25 +0600

    working

--- init.php
+++ init.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Plugin Name: Addons WPT - Specific
+ * Plugin Name: Addons WPT - Jacob
  * Plugin URI: https://wooproducttable.com/
  * Description: WooProductTable Addons Plugin for specific task.
  * Author: Saiful Islam
