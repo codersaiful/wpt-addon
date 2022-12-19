4e3ad005b6003267b4333b6d81e237a4df382af6
Fazle Bari <fazlebarisn@gmail.com>
2022-12-19 15:32:00 +0600

    Remove extra codes

--- inc/functions.php
+++ inc/functions.php
@@ -2,22 +2,6 @@
 /**
  * All important functon will stay here.
  */
-// $dd = rp_wcdpd_get_product_pricing_rules_applicable_to_product(17664,1);
-// var_dump($dd);
-// ob_start();
-// $settings = get_option('rp_wcdpd_settings', array());
-// var_dump($settings);
-// $dd = ob_get_clean();
-// var_dump($dd);
-//  add_filter( 'rp_wcdpd_volume_pricing_table_data' , 'rp_wcdpd_volume_pricing_table_data_callback', 10, 3);
-// $value = get_option( 'rp_wcdpd_settings' );
-// echo '<pre>';
-// // print_r($value);
-// echo '</pre>';
-// // var_dump($value[1]['product_pricing'][0]['quantity_ranges']);
-// var_dump($value[1]['product_pricing'][0]['conditions']);
-// var_dump($value[1]['product_pricing'][1]['conditions']);
-// dd($value[1]['product_pricing'][0]['quantity_ranges']);
 
 $product_id = 17664;
 $variation_id = 19666;
