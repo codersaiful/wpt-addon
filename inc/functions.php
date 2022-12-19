639f51fa607714fb30cdb9e4b31c9727577ee7ed
Saiful Islam <codersaiful@gmail.com>
2022-12-19 15:29:00 +0600

    arrage generatated

--- inc/functions.php
+++ inc/functions.php
@@ -19,58 +19,51 @@
 // var_dump($value[1]['product_pricing'][1]['conditions']);
 // dd($value[1]['product_pricing'][0]['quantity_ranges']);
 
-ob_start();
-$value = get_option( 'rp_wcdpd_settings' );
+$product_id = 17664;
+$variation_id = 19666;
 
-function getValues( $value ){
+ob_start();
+
+
+function get_product_pricing(){
+    $value = get_option( 'rp_wcdpd_settings' );
 	return $value[1]['product_pricing'] ?? [];
 }
 
-$pricingArray = getValues($value);
+$pricingArray = get_product_pricing();
+// $key = array_search($variation_id, array_column($userdb, 'uid'));
+//If Variation_type
+function getIdWiseSetting(){
+    $idWiseSetting = [];
+    $pricingArray = get_product_pricing();
+    foreach($pricingArray as $pricing_key => $eachPricing){
+        $conditionsEachPricing = $eachPricing['conditions'] ?? [];
 
-echo '<pre>';
-print_r($pricingArray);
-echo '</pre>';
-return;
-$product_pricing = getValues( $pricingArray['product_pricing'] );
+        foreach($conditionsEachPricing as $eachCondEchPricing){
+            $type = $eachCondEchPricing['type'] ?? '';
+            
+            $product_variations = $eachCondEchPricing['product_variations'] ?? [];
+            $products = $eachCondEchPricing['products'] ?? [];
+            
+            $quantity_ranges = $eachPricing['quantity_ranges'] ?? [];
+            $ids_collections = [];
+            if($type === 'product__product'){
+                $ids_collections = $products;
+            }elseif($type === 'product__variation'){
+                $ids_collections = $product_variations;
+            }
+            if(empty( $ids_collections ) || ! is_array( $ids_collections )) continue;
 
-$product_variations = getValues($product_pricing['conditions']);
+            
+            foreach($ids_collections as $id_collection){
+                $idWiseSetting[$id_collection] = $quantity_ranges;
+            }
 
-function variaId( $product_variations ){
-
-    if( ! is_array( $product_variations )) return;
-    if( ! isset( $product_variations['product_variations'] )) return;
-	foreach( $product_variations['product_variations'] as $id  ){
-		$id = $id;
-	}
-	return $id;
+        }
+        
+    }
+    return $idWiseSetting;
 }
-
-function quantityRanges( $product_pricing ){
-	foreach( $product_pricing['quantity_ranges'] as $quantity_ranges ){
-		$price = $quantity_ranges;
-	}
-	return $price;
-}
-
-$price = quantityRanges( $product_pricing );
-$id = variaId( $product_variations );
-var_dump($id);
 echo '<pre>';
-print_r($value[1]['product_pricing']);
+print_r(getIdWiseSetting());
 echo '</pre>';
-
-// add_action("woocommerce_after_main_content",function(){
-//     global $product;
-//     echo "HHHHHHHHHHHHHHHHHH<br>";
-//     echo RP_WCDPD_Promotion_Volume_Pricing_Table::maybe_display_pricing_table($product);
-//     echo "HHHHHHHHHHHHHHHHHH";
-//     // var_dump(get_the_ID());
-//     echo rp_wcdpd_display_volume_pricing_table(17392);
-//     echo rp_wcdpd_display_volume_pricing_table(17664);
-//     // var_dump($product->get_id(),$product);
-// });
-// add_filter('rp_wcdpd_volume_pricing_table_data',function($data, $product, $rule){
-//     // var_dump($data, $product, $rule);
-//     return $data;
-// },10, 3);
