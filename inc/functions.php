6eee58ef2efa6e2cf662dede255cf7301a3b4f89
Fazle Bari <fazlebarisn@gmail.com>
2022-12-19 17:28:56 +0600

    remove

--- inc/functions.php
+++ inc/functions.php
@@ -2,17 +2,11 @@
 /**
  * All important functon will stay here.
  */
-// function dd($val){
-// 	echo "<pre>";
-// 		var_dump($val);
-// 	echo "</pre>";
-//  }
 
 $product_id = 17664;
 $variation_id = 19666;
 
 ob_start();
-
 
 function get_product_pricing(){
     $value = get_option( 'rp_wcdpd_settings' );
@@ -67,32 +61,13 @@ function wpt_add_dynamic_price_column( $add_new_col_type, $columns_array, $colum
 }
 add_filter( 'wpto_addnew_col_arr', 'wpt_add_dynamic_price_column', 10, 4 );
 
-
-if( !function_exists( 'codeastrology_dynamic_price_file' ) ){
-    function codeastrology_dynamic_price_file( $file ){
-        $file = __DIR__ . '/../file/dynamic_price.php';
-        //$file = $your_file_location;
-        // dd($file);
-        return $file;
-    }
- }
-//  add_filter( 'wpto_template_loc_item_dynamic_price', 'codeastrology_dynamic_price_file', 10 );
- 
-//  if( !function_exists( 'codeastrology_single_attribute' ) ){
-
-//     function codeastrology_single_attribute( $column_array ) {
-//         dd($column_array);
-//         return $column_array;
-//     }
- 
-//  }
-//  add_filter( 'wpto_default_column_arr', 'codeastrology_single_attribute' );
-
-add_filter( 'wpto_template_folder', 'wpt_custom_pro_change_template_folder', 50, 3 );
+/**
+ * Add file for new type column 
+ */
 function wpt_custom_pro_change_template_folder($items_directory, $keyword, $type){
     if($type == 'dynamic_price'){
         return __DIR__ . '/../file/';
-
     }
     return $items_directory;
 }
+add_filter( 'wpto_template_folder', 'wpt_custom_pro_change_template_folder', 50, 3 );
