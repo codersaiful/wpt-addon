79339e12ae5f0a7b2f480ae8bcd3840a27933c7a
Fazle Bari <fazlebarisn@gmail.com>
2022-12-19 15:31:07 +0600

    will do

--- inc/functions.php
+++ inc/functions.php
@@ -2,3 +2,10 @@
 /**
  * All important functon will stay here.
  */
+
+//  function wpto_add_to_cart_text_callback( $add_to_cart_text_final, $settings, $column_settings, $table_ID, $product ){
+//     $add_to_cart_text_final = "sony";
+//     // dd($add_to_cart_text_final);
+//     return $add_to_cart_text_final;
+//  }
+// add_filter( 'wpto_add_to_cart_text', 'wpto_add_to_cart_text_callback',10,5);
