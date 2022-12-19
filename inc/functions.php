5acec5f01f87f68a83e0f8d8ebc783c0054435b5
Saiful Islam <codersaiful@gmail.com>
2022-12-19 15:30:41 +0600

    ewerwerw

--- inc/functions.php
+++ inc/functions.php
@@ -64,6 +64,6 @@ function getIdWiseSetting(){
     }
     return $idWiseSetting;
 }
-echo '<pre>';
-print_r(getIdWiseSetting());
-echo '</pre>';
+// echo '<pre>';
+// print_r(getIdWiseSetting());
+// echo '</pre>';
