7298253a645ec581ee625ea629def76043234928
Saiful Islam <codersaiful@gmail.com>
2022-12-19 14:24:55 +0600

    deqwd

--- inc/functions.php
+++ inc/functions.php
@@ -20,34 +20,49 @@
 // dd($value[1]['product_pricing'][0]['quantity_ranges']);
 
 
-$value = get_option( 'rp_wcdpd_settings' );
+// $value = get_option( 'rp_wcdpd_settings' );
 
-function getValues( $value ){
-	$asValue = [];
-	foreach( $value as $asValue ){
-		$asValue[] = $asValue;
-	}
-	return $asValue;
-}
+// function getValues( $value ){
+// 	$asValue = [];
+// 	foreach( $value as $asValue ){
+// 		$asValue[] = $asValue;
+// 	}
+// 	return $asValue;
+// }
 
-$pricingArray = getValues($value);
-$product_pricing = getValues( $pricingArray['product_pricing'] );
+// $pricingArray = getValues($value);
+// $product_pricing = getValues( $pricingArray['product_pricing'] );
 
-$product_variations = getValues($product_pricing['conditions']);
+// $product_variations = getValues($product_pricing['conditions']);
 
-function variaId( $product_variations ){
-	foreach( $product_variations['product_variations'] as $id  ){
-		$id = $id;
-	}
-	return $id;
-}
+// function variaId( $product_variations ){
+// 	foreach( $product_variations['product_variations'] as $id  ){
+// 		$id = $id;
+// 	}
+// 	return $id;
+// }
 
-function quantityRanges( $product_pricing ){
-	foreach( $product_pricing['quantity_ranges'] as $quantity_ranges ){
-		$price = $quantity_ranges;
-	}
-	return $price;
-}
+// function quantityRanges( $product_pricing ){
+// 	foreach( $product_pricing['quantity_ranges'] as $quantity_ranges ){
+// 		$price = $quantity_ranges;
+// 	}
+// 	return $price;
+// }
 
-$price = quantityRanges( $product_pricing );
-$id = variaId( $product_variations );
+// $price = quantityRanges( $product_pricing );
+// $id = variaId( $product_variations );
+
+add_action("woocommerce_after_main_content",function(){
+    global $product;
+    echo "HHHHHHHHHHHHHHHHHH<br>";
+    echo RP_WCDPD_Promotion_Volume_Pricing_Table::maybe_display_pricing_table($product);
+    echo "HHHHHHHHHHHHHHHHHH";
+    // var_dump(get_the_ID());
+    echo rp_wcdpd_display_volume_pricing_table(17392);
+    echo rp_wcdpd_display_volume_pricing_table(17664);
+    // var_dump($product->get_id(),$product);
+});
+add_filter('rp_wcdpd_volume_pricing_table_data',function($data, $product, $rule){
+    // var_dump($data, $product, $rule);
+    return $data;
+},10, 3);
