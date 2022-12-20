6a3e06778702978fcf7d201ca12df7f7505c8939
Fazle Bari <fazlebarisn@gmail.com>
2022-12-20 12:23:55 +0600

    add $ simbel

--- file/dynamic_price.php
+++ file/dynamic_price.php
@@ -5,5 +5,5 @@ $my_data = getSpecifcSetting($product_var_id);
 $this_col_data = $my_data[$index] ?? [];
 $price = $this_col_data['pricing_value'] ?? '';
 if(!empty($price)){
-    echo $price;
+    echo '$'.$price;
 }
