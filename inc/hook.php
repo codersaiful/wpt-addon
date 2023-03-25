60d64085fb25afc5e6a8fe7e9bad462c936832ac
Fazle Bari <fazlebarisn@gmail.com>
2023-03-25 11:13:28 +0600

    start working

--- inc/hook.php
+++ inc/hook.php
@@ -15,19 +15,29 @@ use WPT_ADDON\Inc\App\Hook_Base;
 class Hook extends Hook_Base{
 
     public function __construct(){
-
-        
-        $this->action('example_hook');        
-        $this->filter('example_filter');   
+        //$this->action('example_hook');        
+        //add_filter('wpt_query_args' , [$this, 'tableQuery'] );  
+        //add_filter('wpto_table_query_args' , [$this, 'tableQuery'] );  
     }
 
 
-    function example_hook(){
-        echo '<h2>Example Hook</h2>';
-    }
-    function example_filter(){
-        return 'Example Hook';
-    }
+    /**
+     * Here we are changining the default argument of the table
+     * If the table is displying is group product page on then we will chenge the argument
+     * @param [array] $args
+     * @return $args
+     * @author Fazle Bari
+     */
+    public function tableQuery( $args ){
+        //$behavior = $args['behavior'] ?? '';
+       //$args['behavior'] = 'normal';
+       //$args['posts_per_page'] = 2;
+       global $wpdb;
+       $query_vars = isset( $GLOBALS['wp_query']->query_vars ) ? $GLOBALS['wp_query']->query_vars : false;
+       dd( $query_vars );
+       if($query_vars ) return $args;
 
-
+        //dd($args);
+        return $args;
+    }
 }
