e8b0ae9f41a4d3ca6a4f9b233d056ba11110b2c6
Fazle Bari <fazlebarisn@gmail.com>
2023-05-11 17:55:32 +0600

    Rearange codes

--- inc/functions.php
+++ inc/functions.php
@@ -15,20 +15,3 @@ if( !function_exists('wpt_is_pro') ){
         return false;
     }
 }
-
-
-
-
-/**
- * This will add a new product type in 'Product Type' option. 'Product Type' option is in the queary tab. 
- * 
- * @author Fazle Bari ( fazlebarisn@gmail.com)
- */
-function wpt_simple_vari_product_type( $product_types ){
-	$product_types[] = [
-		'value' => 'product_var_and_simple',
-		'label' => __('Variation and Simple Product', 'wpt_pro')
-	];
-    return $product_types;
-}
-add_filter( 'wpt_admin_product_type' , 'wpt_simple_vari_product_type');
--- inc/hook.php
+++ inc/hook.php
@@ -16,64 +16,64 @@ class Hook extends Hook_Base{
 
     public function __construct(){
 
-        
         // $this->action('example_hook');        
-        // $this->filter('example_filter');  
-        
-        //add_filter('wpt_query_args','codeastrology_args_manipulation');
+
+        $this->filter('wpt_admin_product_type');   
         $this->filter('wpt_query_args');   
     }
 
-
-    function example_hook(){
-        echo '<h2>Example Hook</h2>';
+    /**
+     * This will add a new product type in 'Product Type' option. 'Product Type' option is in the queary tab. 
+     * 
+     * @author Fazle Bari <fazlebarisn@gmail.com>
+     */
+    public function wpt_admin_product_type( $product_types ){
+        $product_types[] = [
+            'value' => 'product_var_and_simple',
+            'label' => __('Variation and Simple Product', 'wpt_pro')
+        ];
+        return $product_types;
     }
-    function example_filter(){
-        return 'Example Hook';
-    }
-
 
     /**
      *  Display variable and simple product together
-     * @author Fazle Bari
+     * @author Fazle Bari <fazlebarisn@gmail.com>
      * @author Saiful Islam <codersaiful@gmail.com>
      */
-    function wpt_query_args( $args ){
+    public function wpt_query_args( $args ){
 
         $post_type = $args['post_type'] ?? 'product';
         if( 'product_var_and_simple' !== $post_type ) return $args;
 
         $args['post_type'] = 'product';
 
-    $query = new \WP_Query($args);
-    $p_products = array();
-    if($query->have_posts()):
-        while( $query->have_posts() ): $query->the_post();
-            global $product;
-            $id = get_the_id();
-            if( $product->get_type() !== 'variable' ){
-                $p_products[] = $id;
-            }
+        $query = new \WP_Query($args);
+        $p_products = array();
+        if($query->have_posts()):
+            while( $query->have_posts() ): $query->the_post();
+                global $product;
+                $id = get_the_id();
+                if( $product->get_type() !== 'variable' ){
+                    $p_products[] = $id;
+                }
 
-            if( $product->get_type() == 'variable' ){
-                $variable = new \WC_Product_Variable( $id );
+                if( $product->get_type() == 'variable' ){
+                    $variable = new \WC_Product_Variable( $id );
 
-                $available_variations = $variable->get_available_variations();
-                $variations_id = wp_list_pluck( $available_variations, 'variation_id' );
-                $p_products = array_merge($p_products, $variations_id);
+                    $available_variations = $variable->get_available_variations();
+                    $variations_id = wp_list_pluck( $available_variations, 'variation_id' );
+                    $p_products = array_merge($p_products, $variations_id);
 
-            }
+                }
 
-        endwhile;
-        endif;
-        $args['post_type'] = array('product', 'product_variation',);
-        $args['post__in'] = $p_products;
-        $args['tax_query'] = false;
-        $args['meta_query'] = false;
+            endwhile;
+            endif;
+            $args['post_type'] = array('product', 'product_variation',);
+            $args['post__in'] = $p_products;
+            $args['tax_query'] = false;
+            $args['meta_query'] = false;
 
-    return $args;
-    }
-
-
+        return $args;
+        }
 
 }
