0aabdca70c17f0539b5f8e58ee02a0a02c76be0e
Saiful Islam <codersaiful@gmail.com>
2023-06-08 12:40:02 +0600

    product table design tab new option added

--- inc/functions.php
+++ inc/functions.php
@@ -2,3 +2,22 @@
 /**
  * All important functon will stay here.
  */
+
+// add_filter('wpt_design_tab_fields','wpt_custom_design_tab');
+function wpt_custom_design_tab( $args ){
+    $args['body']['item'][] = [
+        'title' => 'Table Border Width',
+        'selector' => 'tbody tr td',
+        'property' => 'border-width',
+        'type'  => 'text'
+    ];
+    $args['body']['item'][] = [
+        'title' => 'Table Border Width',
+        'selector' => 'tbody tr td',
+        'property' => 'border-style',
+        'value'     => 'solid',
+        'type'  => 'text'
+    ];
+    // var_dump($args['body']['item']);
+    return $args;
+}
