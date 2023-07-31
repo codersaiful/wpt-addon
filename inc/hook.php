073d2af7c7b1924971560990f72bbfe1927882bb
Fazle Bari <fazlebarisn@gmail.com>
2023-07-31 10:27:35 +0600

    Final version for regina

--- inc/hook.php
+++ inc/hook.php
@@ -15,77 +15,58 @@ use WCMMQ_ADDON\Inc\App\Hook_Base;
 class Hook extends Hook_Base{
 
     public function __construct(){
-        $this->action('woocommerce_single_product_summary');        
+        $this->filter('cwg_default_allowed_status', 10, 2 );        
         $this->filter('woocommerce_product_add_to_cart_text', 10, 2);   
     }
 
-
     /**
-     * add a input box on the single product page 
+     * There is a filter hook in 'Back In Stock Notifier' plugin. That hook is returing the stock status. Notifacation box will appar if 
+     * product is out of stock. Here i am changing  stock status. 
      * @author Fazle Bari <fazlebarisn@gmail.com>
      */
-    public function woocommerce_single_product_summary(){
-        global $wpdb;
-        if ( isset( $_POST['wcmmq_stock_email'] ) && isset( $_POST['product_id'] ) && isset( $_POST['wcmmq_stock_email_nonce'] ) && wp_verify_nonce( $_POST['wcmmq_stock_email_nonce'], 'wcmmq_stock_email' ) ) {
-            $email = sanitize_email( $_POST['wcmmq_stock_email'] );
-            $product_id = absint( $_POST['product_id'] );
-            $saved = false;
-            if(! empty($email) ){
-                $table_name = $wpdb->prefix . $this->notify_table_name; //'wcmmq_low_stock_emails' 
-                $saved = wcmmq_save_email_to_database( $email, $product_id, $table_name );
-            }
+    public function cwg_default_allowed_status( $status, $product ){
 
-            if ( $saved ) {
-                $msessage = "Thank you! We will notify you when the product is available.";
-                // wc_add_notice( $msessage, 'success' );
-                ?>
-                <div class="woocommerce">
-                    <div class="woocommerce-message" role="alert"><?php echo esc_html( $msessage ); ?></div>
-                </div>
-                
-                <?php 
-                // echo '<p class="wcmmq-message wcmmq-message-success">' . $msessage . '</p>';
-            } else {
-                $msessage = "There was an error. Please try again later.";
-                // wc_add_notice( $msessage, 'error' );
-                ?>
-                <div class="woocommerce">
-                    <ul class="woocommerce-error" role="alert">
-                        <li><?php echo esc_html( $msessage ); ?></li>
-                    </ul>
-                </div>
-                <?php 
-                // echo '<p class="wcmmq-message wcmmq-message-error">' . $msessage . '</p>';
-            }
-        }
-
-        global $product;
         $product_data= $product->get_data();
         $product_id= $product->get_id();
-
         $stock_status = $product_data['stock_status'];
         $stock_qty = $product_data['stock_quantity'];
 
         if( ! defined( 'WC_MMQ_PREFIX' ) ){
             define('WC_MMQ_PREFIX', '');
         }
-        
-        // $min_quantity = get_post_meta($product_id, 'min_quantity', true);
+
         $min_quantity = get_post_meta($product_id, WC_MMQ_PREFIX . 'min_quantity', true);
-        
-        if( ! empty( $min_quantity ) && ! empty( $stock_qty ) && $min_quantity > $stock_qty ) :
-            remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 30 );
-            ?>
-                <div class="wcmmq-notify">
-                    <form method="post" action="">
-                        <input class="wcmmq-notify-inputbox" type="email" name="wcmmq_stock_email" placeholder="Enter your email" required>
-                        <input class="wcmmq-notify-button" type="submit" value="Notify me when available">
-                        <input type="hidden" name="product_id" value="<?php echo $product->get_id(); ?>">
-                        <?php wp_nonce_field( 'wcmmq_stock_email', 'wcmmq_stock_email_nonce' ); ?>
-                    </form>
-                </div>
-            <?php
-        endif;
+    
+        if( $stock_qty  < $min_quantity && 'instock' == $stock_status ){
+            $status = ['instock'];
+            add_action('wp_footer', [ $this , 'hide_add_to_cart'] );
+        }
+    
+        return $status;
+
+    }
+
+    /**
+     * I am adding some css to hide add to cart and 'in stock' text and add 'out of stock' text
+     *  @author Fazle Bari <fazlebarisn@gmail.com>
+     */
+    public function hide_add_to_cart(){
+        ?>
+        <style>
+            button.single_add_to_cart_button.button.alt {
+                display: none !important;
+            }
+			p.stock.in-stock {
+				display: none;
+			}
+			.single-product-summary .quantity::before {
+				content: "Out of stock";
+				font-size: 13px;
+				font-weight: normal;
+				display: inline;
+			}
+        </style>
+        <?php
     }
 
     /**
