e909eac61528f04e760ea0f08bef8968a6ae79a9
Fazle Bari <fazlebarisn@gmail.com>
2023-09-20 14:19:10 +0600

    add dd function

--- inc/functions.php
+++ inc/functions.php
@@ -2,3 +2,19 @@
 /**
  * All important functon will stay here.
  */
+
+  /**
+ * Only for developer
+ * @author Fazle Bari <fazlebarisn@gmail.com>
+ */
+if( ! function_exists('dd') ){
+	function dd( ...$vals){
+		if( ! empty($vals) && is_array($vals) ){
+			foreach($vals as $val ){
+				echo "<pre>";
+				    var_dump($val);
+				echo "</pre>";
+			}
+		}
+	}
+}
